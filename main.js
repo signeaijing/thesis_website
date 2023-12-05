ca748480ebe2454c7e9e24aeb495c8b044ada43c
sineaijing <sajrhansen@gmail.com>
2023-12-05 21:02:44 +0000

    fourth commit

--- main.js
+++ main.js
@@ -17,15 +17,19 @@ document.body.appendChild(renderer.domElement);
 //renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
 // _____________________INSTANTIALIZE OBJECTS______________________// 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 3000);
 const controls = new OrbitControls(camera, renderer.domElement);
 camera.position.set( 300, 100, 500 );
 controls.update();
 //camera.position.z = 500;
 //camera.position.set(-250,-50,-250);
-const scene = new THREE.Scene();
+
+// adding two scenes to be able to shift between them
+const originalScene = new THREE.Scene();
+const queerScene = new THREE.Scene(); 
 const myTextureLoader = new THREE.TextureLoader();
-scene.add(camera);
+originalScene.add(camera);
+queerScene.add(camera);
 
 // canvas.addEventListener('mousemove', getMouse, false);    // hvad skal jeg kalde den her på? renderer=  
 
@@ -35,67 +39,67 @@ controls.maxDistance = 1300;
 
 //_______________________ AXES HELPER________________________// 
 const axesHelper = new THREE.AxesHelper(100);
-scene.add(axesHelper);
-
+originalScene.add(axesHelper);
+queerScene.add(axesHelper);
 
 //________ LOAD IN LATENT VECTORS ORIGINAL DATA_________//
-//async function getOGVectors(){
- // const response = await fetch("/latent_vectors_aegte.json");      // get the specific json file  
-  //const fetchedData = await response.json();        // tells that we are fetching json 
-  //console.log(fetchedData)              // check that the data is right 
-  //return fetchedData;
-//};
+async function getOGVectors(){
+  const response = await fetch("/latent_vectors_aegte.json");      // get the specific json file  
+  const fetchedData = await response.json();        // tells that we are fetching json 
+  console.log('data in',fetchedData);        // check that the data is right 
+  return fetchedData;
+};
 
-//let originalData;     // declare the data variable in outer scope, needs to be let as it will alter when we put data into it
+let originalData;     // declare the data variable in outer scope, needs to be let as it will alter when we put data into it
 
 // call fetch function and assign data 
-//getOGVectors()
- // .then(fetchedData => {
-  //  originalData = fetchedData; // Store the fetched data in the outer variable
-   // console.log('hello',fetchedData); // check that the data is right 
-    //OGData(originalData);      // call here ecause getvectors is async
-  //});
+getOGVectors()
+  .then(fetchedData => {
+    originalData = fetchedData; // Store the fetched data in the outer variable
+    console.log('hello',fetchedData); // check that the data is right 
+    OGData(originalData);      // call here ecause getvectors is async
+  });
 
 // ______________LOAD IN QUEER VECTORS______________// 
+
 async function getQueerVectors() {
   const queerResponse = await fetch('/interpolated_latent_vectors.json');
   const fetchedQueerData = await queerResponse.json();
-  console.log(fetchedQueerData);
+  console.log('data here',fetchedQueerData);
   return fetchedQueerData;
 }
 
 let queerData;
 
-// call new fetch function and assign data 
+// call new fetch function and assign data
 getQueerVectors()
   .then(async fetchedQueerData => {
-    queerData = fetchedQueerData; // Store the fetched data in the outer variable
+   queerData = fetchedQueerData; // Store the fetched data in the outer variable
     console.log('hello queers',fetchedQueerData); // check that the data is right 
     interpolatedData(queerData);      // call here ecause getvectors is async
   });
 
-
 //___________CREATE_THREE_JS_OBJECTS_FOR_VECTORS___________// 
-// Function to load data into Three.js using the fetched 'data'
-//function OGData(data1) {
- // const object1 = new THREE.Object3D();      // create empty object  
-
-//data1.forEach(vector1 => {     // create new object for each vector in /the data variable
- //   const [x, y, z] = vector1;
+ //Function to load data into Three.js using the fetched 'data'
+function OGData(data1) {
+  const object1 = new THREE.Object3D();      // create empty object 
+data1.forEach(vector1 => {     // create new object for each vector in /the data variable
+    const [x, y, z] = vector1;
     //const geometry = new THREE.CapsuleGeometry(5, 15, 32, 20);
-   // const geometry1 = new THREE.SphereGeometry(5,32,32);
-    //7const points1 = [];
-    //for ( let i = 0; i < 10; i ++ ) {
-     // points1.push( new THREE.Vector2( Math.sin( i * 0.2 ) * 5 + 5, ( i - 5 ) * 2 ) );
- // }
+    const geometry1 = new THREE.SphereGeometry(5,32,32);
+    const points1 = [];
+    for ( let i = 0; i < 10; i ++ ) {
+      points1.push( new THREE.Vector2( Math.sin( i * 0.2 ) * 5 + 5, ( i - 5 ) * 2 ) );
+  }
     //const geometry = new THREE.LatheGeometry( points );
-   // const material1 = new THREE.MeshBasicMaterial({ color: '#00ff83' });
-    //const mesh1 = new THREE.Mesh(geometry1, material1);
-    //mesh1.position.set(x, y, z);      // place the vectors / data points in the scene in relation to each other
-    //object1.add(mesh1);
-  //});
-  //scene.add(object1); // Add the object containing spheres to the scene
-//}
+    const material1 = new THREE.MeshBasicMaterial({ color: '#00e600' });
+    const mesh1 = new THREE.Mesh(geometry1, material1);
+    mesh1.position.set(x, y, z);      // place the vectors / data points in the scene in relation to each other
+    object1.add(mesh1);
+  });
+  originalScene.add(object1); // Add the object containing spheres to the scene
+  console.log('hello queeres, im also here');
+}
 
 // function to load the interpolated data intro Three.js using the fetched queer data 
 function interpolatedData(data2){
@@ -112,39 +116,64 @@ function interpolatedData(data2){
     mesh2.position.set(h, m, k);
     object2.add(mesh2);
   });
-  scene.add(object2);        // added to the same scene
+  queerScene.add(object2);        // added to the same scene
   console.log('helloqueers, im here!');
 } ;
+
+// this will use the points to create meshes: CAN BE ADDED LATER IF TIME!! 
+//const points2 = [];
+  //  for (let j = 0; j < 10; j ++){
+    //  points2.push(new THREE.Vector2(Math.sin(j * 0.2) * 5 + 5, (j - 5) * 2));
+    //}
+
+    //const shape = new THREE.Shape(points2);
+    //const geometry2 = new THREE.ShapeGeometry(shape); // Create geometry from the shape
+
+//_________________ADD_USER_INPUT_________________// this was created with the help of chatgpt 
+
+
+let sceneState = 'originalScene';     // initialize the scene to one state 
+
+// Double-click event listener
+function toggleScenes() {
+  if (sceneState === 'queerScene') {
+    // if double click and originalscene then go to queerscene
+    originalScene.visible = false;     
+    queerScene.visible = true;
+    sceneState = 'queerScene';
+    
+  } else {
+    // if double click and queer scene then go to original scene
+    queerScene.visible = false;    
+    originalScene.visible = true;
+    sceneState = 'originalScene';
+    console.log('togelelle');
+  };
+};
+// Add event listener for double-click
+document.addEventListener('dblclick', toggleScenes);
+
+// need to use hirarchical positioning here in order to move the latent space, when something moves 
 
 
 //__________________LIGHT_____________________// 
 //const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-const ambientlight = new THREE.AmbientLight( 0x404040 ); // soft white light
-scene.add(ambientlight);
+const ambientlight = new THREE.AmbientLight( 0x404040 ); // soft white lighto
+originalScene.add(ambientlight);
+queerScene.add(ambientlight);
 const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
-scene.add(directionalLight);
-
-// need to use hirarchical positioning here in order to move the latent space, when something moves 
+originalScene.add(directionalLight);
+queerScene.add(ambientlight);
 
 //_______ANIMATE______// 
 function animate() {
   requestAnimationFrame(animate);
   // Update objects, camera, or any other animations here
-  renderer.render(scene, camera);
+  renderer.render(originalScene, camera);
+  renderer.render(queerScene, camera)
 }
 
 animate();
-
-
-
-
-
-
-
-
-
-
- 
 
 
 
@@ -166,3 +195,50 @@ animate();
 
 
 
+//__OLD FUNCTIONS TO LOAD IN QUEER DATA __
+/*async function getQueerVectors() {
+  const queerResponse = await fetch('/interpolated_latent_vectors.json');
+  const fetchedQueerData = await queerResponse.json();
+  console.log(fetchedQueerData);
+  return fetchedQueerData;
+}
+
+let queerData;
+
+// call new fetch function and assign data
+getQueerVectors()
+  .then(async fetchedQueerData => {
+   queerData = fetchedQueerData; // Store the fetched data in the outer variable
+    console.log('hello queers',fetchedQueerData); // check that the data is right 
+    interpolatedData(queerData);      // call here ecause getvectors is async
+  });*/
+
+
+  /*
+function mouseDoubleClick(event){
+  if (event.detail ===2) {     // if mouse doubleclicked 
+    sceneState = (sceneState === 'original') ? 'queer': 'original';
+    updateScene();
+  }}
+
+  document.addEventListener('dblclick', mouseDoubleClick);  // FUNCTION CALL IN THE END HERE!!! 
+
+function updateScene(){
+  clearScene();      //remove the meshes in the scene
+  if (sceneState === 'original'){
+    OGData(originalData);
+  } else if (sceneState === ' queer'){
+    interpolatedData(queerData);  
+  }
+}
+
+function clearScene(){
+    // Remove all objects from the scene
+    scene.traverse(object =>) {
+      if (object instanceof THREE.Mesh) {
+        scene.remove(object);
+      }
+    }}
+
+
+updateScene();*/
