9bf595e7b4ef9da0b470e3f0392e9507480d2c1a
sineaijing <sajrhansen@gmail.com>
2023-12-08 14:25:28 +0000

    14th commit

--- main.js
+++ main.js
@@ -164,8 +164,6 @@ originalScene.add(directionalLight);
 
 //________________ADD_MUSIC_________________//
 
-
-
 const listener = new THREE.AudioListener();
 const audioLoader = new THREE.AudioLoader();
 const audio = new THREE.Audio(listener);
